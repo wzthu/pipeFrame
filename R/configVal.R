c8620d658a3e7ec7c7f715b9267f75de4ff063f5
Zheng Wei <wzweizheng@qq.com>
2019-03-08 15:19:19 -0800

    move back BSgenom configure function to pipeFramer

--- R/configVal.R
+++ R/configVal.R
@@ -162,6 +162,39 @@ getRefRc <- function(refName){
 
 
 
+
+checkAndInstallBSgenome <- function(resultDirPaths){
+    genome <- getGenome()
+    bsgenomename<- BSgenome::available.genomes()[grepl(paste0(genome,"$"),BSgenome::available.genomes())]
+    if(length(bsgenomename)==0){
+        message()
+        stop("There is no BSgenome support for this genome")
+    }
+    bsgenomeinstall <- BSgenome::installed.genomes()[grepl(paste0(genome,"$"),BSgenome::installed.genomes())]
+    if(length(bsgenomeinstall)==0){
+        message(paste("BSgenome for ",genome,"has not been installed,"))
+        message("begin to install ...")
+        BiocManager::install(bsgenomename)
+    }
+}
+
+checkAndInstallGenomeFa <- function(resultDirPaths){
+    outFile <- resultDirPaths
+    bsgenome<-getRefRc("bsgenome")
+    if(!is(bsgenome, "BSgenome")){stop("The variable 'bsgenome' is not a BSgenome")}
+    append <- FALSE
+    for(chrT in seqnames(bsgenome)){
+        if(is.null(masks(bsgenome[[chrT]])))
+            chrSeq <- DNAStringSet(bsgenome[[chrT]])
+        else
+            chrSeq <- DNAStringSet(injectHardMask(bsgenome[[chrT]], letter="N"))
+        names(chrSeq) <- chrT
+        writeXStringSet(chrSeq, filepath=outFile, format="fasta", append=append)
+        append <- TRUE
+    }
+    return(outFile)
+}
+
 checkAndInstall <- function(check = TRUE, ...){
 }
 
