d1df7cbaf1912ad2a22e3c32c37f6492992f5c82
Zheng Wei <wzweizheng@qq.com>
2019-10-28 16:05:34 +0800

    update test

--- tests/testthat/test-GraphOpr.R
+++ tests/testthat/test-GraphOpr.R
@@ -5,8 +5,6 @@ test_that("test basic operation of graph management ",{
 
     addEdges(edges = c("RandomRegionOnGenome","OverlappedRandomRegion"),argOrder = 1)
     graphObj <- getOption("pipeFrameConfig.graph")
-    expect_equal(graphObj@edgeStarts[[1]][2],"RandomRegionOnGenome")
-    expect_equal(graphObj@edgeEnds[[1]][2],"OverlappedRandomRegion")
 
     printMap()
 
