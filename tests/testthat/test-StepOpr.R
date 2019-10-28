642bd0a6fbf997b40736eea5bc46bdba813ef361
Zheng Wei <wzweizheng@qq.com>
2019-10-28 16:20:12 +0800

    remove old test

--- tests/testthat/test-StepOpr.R
+++ tests/testthat/test-StepOpr.R
@@ -199,8 +199,6 @@ test_that("test basic operation of class Step ",{
     addEdges(edges = c("RandomRegionOnGenome","OverlappedRandomRegion"),
              argOrder = 1)
     graphObj <- getOption("pipeFrameConfig.graph")
-    expect_equal(graphObj@edgeStarts[[1]][2],"RandomRegionOnGenome")
-    expect_equal(graphObj@edgeEnds[[1]][2],"OverlappedRandomRegion")
     # begin to test pipeline
     setGenome("hg19")
     expect_equal(getGenome(),"hg19")
